bee849629d4701cdee15d2799310fca97f2a54f9
Suraj Kumar <suraj28j@gmail.com>
2024-10-27 22:20:16 +0530

    login controller created

--- controller/authController.js
+++ controller/authController.js
@@ -28,3 +28,21 @@ export const registerUser = async (req, res, next) => {
     }
 }
 
+export const loginUser = async (req, res, next) => {
+    const { email } = req.body;
+    try {
+        let user = await User.findById(email);
+        if (!user) {
+            return res.status(400).json({ success: false, message: "Email not exists" })
+        }
+        const isPasswordMatch = await bcrypt.compare(req.body, user.password);
+        if (!isPasswordMatch) {
+            return res.status(401).json({ success: false, message: "Incorrect Password" });
+        }
+        res.status(200).json({ success: true, message: "User Login successfully" })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: "Internal server error" });
+    }
+}
+
